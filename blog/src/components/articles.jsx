bdd3d74065e9ebb588a64aa158512cadaa924e0f
mahyaam <7mahya.amiri@gmail.com>
2021-08-03 21:20:21 +0430

    Pagination

--- blog/src/components/articles.jsx
+++ blog/src/components/articles.jsx
@@ -1,44 +1,64 @@
 import React, { Component } from "react";
 import { getArticles } from "../services/articlesService";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 
 class Articles extends Component {
   state = {
     articles: getArticles(),
+    pageSize: 3,
+    currentPage: 1,
   };
 
   handleContinue = (article) => {
     console.log(article);
   };
 
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
+    const { length: count } = this.state.articles;
+    const { pageSize, currentPage, articles: allArticles } = this.state;
+    const articles = paginate(allArticles, currentPage, pageSize);
+
     return (
-      <table className="table" dir="rtl">
-        <thead>
-          <tr>
-            <td>عنوان مقاله</td>
-            <td>دسته بندی مقاله</td>
-            <td>محتوای مقاله</td>
-            <td></td>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.articles.map((article) => (
-            <tr key={article.id}>
-              <td>{article.title}</td>
-              <td>{article.category}</td>
-              <td>{article.content}</td>
-              <td>
-                <button
-                  onClick={() => this.handleContinue(article)}
-                  className="btn btn-info btn sm"
-                >
-                  ادامه مقاله
-                </button>
-              </td>
+      <React.Fragment>
+        <table className="table" dir="rtl">
+          <thead>
+            <tr>
+              <td>عنوان مقاله</td>
+              <td>دسته بندی مقاله</td>
+              <td>محتوای مقاله</td>
+              <td></td>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {articles.map((article) => (
+              <tr key={article.id}>
+                <td>{article.title}</td>
+                <td>{article.category}</td>
+                <td>{article.content}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleContinue(article)}
+                    className="btn btn-info btn sm"
+                  >
+                    ادامه مقاله
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        ></Pagination>
+      </React.Fragment>
     );
   }
 }
