0d20c43c797a77077d8b9dae3e70c0a91d6ad3ee
mahyaam <7mahya.amiri@gmail.com>
2021-08-06 23:51:30 +0430

    updated

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Navigation from "./components/Navigation";
+import Navigation from "./components/common/Navigation";
 import "./App.css";
 
 function App() {
--- blog/src/components/common/Navigation.jsx
+++ blog/src/components/common/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Articles from "./Articles";
+import Articles from "../Articles";
 
 export default function App() {
   return (
