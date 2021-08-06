5eb54e0a3aa712d87c0ecbb74fb2c5eedfc1ca15
mahyaam <7mahya.amiri@gmail.com>
2021-08-06 23:52:08 +0430

    added MVC

---
+++ BlogProject/BlogProject/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ BlogProject/BlogProject/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UsreName { get; set; }
+        public string Password { get; set; }
+    }
+}
