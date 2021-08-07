5a3fae9bd8daa60f14660fdc92fc1f8ba059c98e
mahyaam <7mahya.amiri@gmail.com>
2021-08-07 19:58:02 +0430

    Updated

--- BlogProject/BlogProject/Models/User.cs
+++ BlogProject/BlogProject/Models/User.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 
 namespace BlogProject.Models
 {
-    public class User
+    public class User: Entity
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string UsreName { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
     }
 }
