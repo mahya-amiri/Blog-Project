a0e8fd6d5d6a4d465e37dc66cedf0f187819aa18
mahyaam <7mahya.amiri@gmail.com>
2021-08-08 11:13:32 +0430

    EF Code-First

--- BlogProject/BlogProject/Models/Context.cs
+++ BlogProject/BlogProject/Models/Context.cs
@@ -8,11 +8,10 @@ namespace BlogProject.Models
 {
     public class Context: DbContext
     {
-        public DbSet<Admin> Admins { get; set; }
+        
         public DbSet<Category> Categories { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Post> Posts { get; set; }
-        public DbSet<Publisher> Publishers { get; set; }
         public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
