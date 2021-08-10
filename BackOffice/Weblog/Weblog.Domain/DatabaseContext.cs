7276155c09a3d848030a3cbb2814e50dcecb25d3
mahyaam <7mahya.amiri@gmail.com>
2021-08-11 00:00:05 +0430

    Chapter01

---
+++ BackOffice/Weblog/Weblog.Domain/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Weblog.Domain.Models;
+
+namespace Weblog.Domain
+{
+    public class DatabaseContext: DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Weblog");
+        }
+        //base.OnConfiguring(optionsBuilder);
+    }
+}
---
+++ BackOffice/Weblog/Weblog.Domain/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Weblog.Domain.Models
+{
+    [Table("Categories")]
+
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+        public List<Article> Articles { get; set; }
+        public Category() 
+        {
+        }
+    }
+}
