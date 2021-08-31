5ac8b4bf710a8766b517baad8e8933c23594f0f6
mahyaam <7mahya.amiri@gmail.com>
2021-08-31 23:21:59 +0430

    Chapter13

--- BackOffice/Weblog/Weblog.Domain/Models/User.cs
+++ BackOffice/Weblog/Weblog.Domain/Models/User.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Weblog.Domain.Models
 {
@@ -23,7 +26,6 @@ namespace Weblog.Domain.Models
         public string? Token { get; set; }
         public List<Article> Articles { get; }
         public List<Comment> Comments { get; }
-
         public User()
         {
 
@@ -31,47 +33,44 @@ namespace Weblog.Domain.Models
 
         public User(string name, string email, string password)
         {
-            // Validations for Name
             if (name == null)
             {
                 throw new Exception("نام کاربر را وارد کنید");
             }
-            if (name.Length >= 50)
-            {
-                throw new Exception("نام کاربر نمی تواند بیشتر از 50 کاراکتر باشد");
-            }
-
-            // Validations for Email
             if (email == null)
             {
                 throw new Exception("ایمیل کاربر را وارد کنید");
+            }
+
+            if (name.Length >= 50)
+            {
+                throw new Exception("نام کاربر نمی تواند بیشتر از 50 کاراکتر باشد");
             }
             if (email.Length >= 200)
             {
                 throw new Exception("ایمیل کاربر نمی تواند بیشتر از 200 کاراکتر باشد");
             }
-            var validEmail = Regex.Match(email, @"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)");
-            if (!validEmail.Success)
-            {
-                throw new Exception("آدرس ایمیل وارد شده معتبر نمی باشد");
-            }
-            var _db = new DatabaseContext();
-            var count = _db.Users.Count(x => x.Email == email);
-            if (count > 0)
-            {
-                throw new Exception("آدرس ایمیل وارد شده تکراری می باشد");
-            }
-
             if (password.Length <= 6)
             {
                 throw new Exception("کلمه عبور باید بیشتر از 6 کاراکتر باشد");
             }
 
-            // Validations for Password
+            var validEmail = Regex.Match(email, @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$");
+            if (!validEmail.Success)
+            {
+                throw new Exception("آدرس ایمیل وارد شده معتبر نمی باشد");
+            }
             var validPassword = Regex.Match(password, @"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$");
             if (!validPassword.Success)
             {
                 throw new Exception("رمز عبور باید شامل حروف، عدد و حداقل یک کاراکتر خاص باشد");
+            }
+
+            var _db = new DatabaseContext();
+            var duplicate = _db.Users.Count(x => x.Email == email);
+            if (duplicate > 0)
+            {
+                throw new Exception("آدرس ایمیل وارد شده تکراری می باشد");
             }
 
             this.Name = name;
@@ -79,41 +78,133 @@ namespace Weblog.Domain.Models
             this.Password = GetHashString(password);
         }
 
-        public void Update(string name, string email)
+        public void Update(string name, string email, string newPassword, string currentPassword)
         {
-            // Validations for Name
             if (name == null)
             {
                 throw new Exception("نام کاربر را وارد کنید");
             }
+            if (email == null)
+            {
+                throw new Exception("ایمیل کاربر را وارد کنید");
+            }
+
             if (name.Length >= 50)
             {
                 throw new Exception("نام کاربر نمی تواند بیشتر از 50 کاراکتر باشد");
             }
 
-            // Validations for Email
-            if (email == null)
-            {
-                throw new Exception("ایمیل کاربر را وارد کنید");
-            }
             if (email.Length >= 200)
             {
                 throw new Exception("ایمیل کاربر نمی تواند بیشتر از 200 کاراکتر باشد");
             }
-            var validEmail = Regex.Match(email, @"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)");
+
+            var validEmail = Regex.Match(email, @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$");
             if (!validEmail.Success)
             {
                 throw new Exception("آدرس ایمیل وارد شده معتبر نمی باشد");
             }
+
             var _db = new DatabaseContext();
-            var count = _db.Users.Count(x => x.Email == email && x.Id != this.Id);
-            if (count > 0)
+            var duplicate = _db.Users.Count(x => x.Email == email && x.Id != this.Id);
+            if (duplicate > 0)
             {
                 throw new Exception("آدرس ایمیل وارد شده تکراری می باشد");
+            }
+
+            if (newPassword != null)
+            {
+
+                if (GetHashString(currentPassword) != this.Password)
+                {
+                    throw new Exception("رمز عبور فعلی اشتباه می باشد");
+                }
+
+                if (newPassword.Length <= 6)
+                {
+                    throw new Exception("رمز عبور باید بیشتر از 6 کاراکتر باشد");
+                }
+
+                var validPassword = Regex.Match(newPassword, @"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$");
+                if (!validPassword.Success)
+                {
+                    throw new Exception("رمز عبور باید شامل حروف، عدد و حداقل یک کاراکتر خاص باشد");
+                }
+                this.Password = GetHashString(newPassword);
+
             }
 
             this.Name = name;
             this.Email = email;
         }
+
+        public void UpdateByAdmin(string name, string email, string newPassword)
+        {
+            if (name == null)
+            {
+                throw new Exception("نام کاربر را وارد کنید");
+            }
+            if (email == null)
+            {
+                throw new Exception("ایمیل کاربر را وارد کنید");
+            }
+
+            if (name.Length >= 50)
+            {
+                throw new Exception("نام کاربر نمی تواند بیشتر از 50 کاراکتر باشد");
+            }
+            if (email.Length >= 200)
+            {
+                throw new Exception("ایمیل کاربر نمی تواند بیشتر از 200 کاراکتر باشد");
+            }
+
+            var validEmail = Regex.Match(email, @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$");
+            if (!validEmail.Success)
+            {
+                throw new Exception("آدرس ایمیل وارد شده معتبر نمی باشد");
+            }
+            var _db = new DatabaseContext();
+            var duplicate = _db.Users.Count(x => x.Email == email && x.Id != this.Id);
+            if (duplicate > 0)
+            {
+                throw new Exception("آدرس ایمیل وارد شده تکراری می باشد");
+            }
+
+            if (newPassword != null)
+            {
+
+                if (newPassword.Length <= 6)
+                {
+                    throw new Exception("رمز عبور باید بیشتر از 6 کاراکتر باشد");
+                }
+
+                var validPassword = Regex.Match(newPassword, @"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$");
+                if (!validPassword.Success)
+                {
+                    throw new Exception("رمز عبور باید شامل حروف، عدد و حداقل یک کاراکتر خاص باشد");
+                }
+                this.Password = GetHashString(newPassword);
+
+            }
+
+            this.Name = name;
+            this.Email = email;
+        }
+
+
+        public static byte[] GetHash(string inputString)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+        public static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(inputString))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
+        }
     }
+
 }
--- BackOffice/Weblog/Weblog/Requests/CreateUserRequest.cs
+++ BackOffice/Weblog/Weblog/Requests/CreateUserRequest.cs
@@ -3,16 +3,18 @@
     public class CreateUserRequest
     {
         public string Name { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
 
         public CreateUserRequest()
         {
 
         }
-        public CreateUserRequest(string name, string email)
+        public CreateUserRequest(string name, string email, string password)
         {
             Name = name;
-            Email = email;
+            Email = email; 
+            Password = password;
         }
     }
 }
